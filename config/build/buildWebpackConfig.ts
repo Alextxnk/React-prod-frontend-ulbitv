86f02aa147e937f6a7fce5f0532e8b758cfac046
Alextxnk <alexsolov766@gmail.com>
2023-09-06 18:07:53 +0300

    add new ui components

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -14,7 +14,7 @@ export const buildWebpackConfig = (
       mode: mode,
       entry: paths.entry,
       module: {
-         rules: buildLoaders()
+         rules: buildLoaders(options)
       },
       resolve: buildResolvers(),
       output: {
---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+// глобальная декларация
+declare module '*.scss' {
+   interface IClassNames {
+      [className: string]: string;
+   }
+   const classNames: IClassNames;
+   export = classNames;
+}
