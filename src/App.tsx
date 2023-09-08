e79ce456662e4988c078cd6feecdd187d63b5766
Alextxnk <alexsolov766@gmail.com>
2023-09-08 17:19:32 +0300

    add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Counter from './components/ui/Counter/Counter';
 import './index.scss';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
 
 const App = () => {
    return (
       <div className='app'>
          <Counter />
+         <Link to='/'>Главная</Link>
+         <Link to='/about'>О сайте</Link>
+         <Routes>
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/' element={<MainPage />} />
+         </Routes>
       </div>
    );
 };
