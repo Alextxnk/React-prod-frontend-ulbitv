debf93f7572c01d22c2bc5f654458d660692382c
Alextxnk <alexsolov766@gmail.com>
2023-09-15 14:54:43 +0300

    add route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import Counter from 'components/ui/Counter/Counter';
 import './styles/index.scss';
-import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import Chart from 'components/ui/Chart/Chart';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 const App = () => {
    const { theme, toggleTheme } = useTheme();
@@ -21,13 +19,7 @@ const App = () => {
             <div>
                <button onClick={toggleTheme}>Сменить тему</button>
             </div>
-
-            <Suspense fallback={<div>Loading...</div>}>
-               <Routes>
-                  <Route path='/about' element={<AboutPage />} />
-                  <Route path='/' element={<MainPage />} />
-               </Routes>
-            </Suspense>
+            <AppRouter />
          </div>
          {/* <Chart /> */}
       </>
