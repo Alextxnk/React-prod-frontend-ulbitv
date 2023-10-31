1e92b36cb8ef7ddb4c3538b74c31341b42cda343
Alextxnk <alexsolov766@gmail.com>
2023-10-31 16:30:19 +0300

    change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,7 @@ module.exports = {
       'react/no-array-index-key': 'warn',
       'no-return-assign': 'warn',
       'no-use-before-define': 'warn',
-      'max-len': 'warn'
+      'max-len': ['warn', { ignoreComments: true }]
    },
    globals: {
       __IS_DEV__: true
